619be3846c2b5864733164d549977a01f5f2b3a3
Zulkifli Mohamed <mr.putera@gmail.com>
2016-11-09 09:25:56 +0800

    Kemaskini Table untuk Migrations

--- database/migrations/2016_10_01_000000_create_smartteamaktiviti_table.php
+++ database/migrations/2016_10_01_000000_create_smartteamaktiviti_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateAktivitismartteamTable extends Migration
+class CreateSmartteamAktivitiTable extends Migration
 {
     /**
      * Run the migrations.
